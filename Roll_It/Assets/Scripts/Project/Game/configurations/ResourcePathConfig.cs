727308d058d1223967781339712c553ba9de4876
vfa-DungLN <vfa.dungnl@gmail.com>
2020-11-26 10:31:45 +0700

    setup project

--- Roll_It/Assets/Scripts/Project/Game/configurations/ResourcePathConfig.cs
+++ Roll_It/Assets/Scripts/Project/Game/configurations/ResourcePathConfig.cs
@@ -4,23 +4,7 @@ using UnityEngine;
 
 public class ResourcePathConfig
 {
-    public const string CharaterPath = "Models/Prefabs/GameDataInfomations/Charaters/";
-    public const string MapFolderPath = "Models/Prefabs/Maps/";
-    public const string UIColorSliderImgPath = "Prefabs/UI/ColorSliderImgs/";
-
-    public const string UIColorViewPath = "UI/ColorSliderPrefabs/ColorSliderView";
     public const string GameScreenViewPath = "Commons/Game/GameScreenView";
     public const string GameResultScreenViewPath = "UI/Screen/Result/ResultScreenView";
     public const string GameViewPath = "Commons/Game/GameView";
-    public const string GameDataPath = "GameDataInfomations/Enviroments/GameDataMaster";
-
-    public const string MapReviewFolderPath = "ReviewModels/Prefabs/Maps/";
-
-
-    public const string CharaterDataFolderPath = "GameDataInfomations/Charaters/";
-    public const string CharaterViewFolderPath = "Models/Prefabs/";
-
-    public const string MapLevelDataFolderPath = "GameDataInfomations/Eniroments/";
-
-
 }
