f8a4cf88abe98a90a63636b84fc62e16674569cc
vfa-DungLN <vfa.dungnl@gmail.com>
2020-11-26 14:22:37 +0700

    #Roll_it : Simple demo

--- Roll_It/Assets/Scripts/Project/Game/Common/View/GameView.cs
+++ Roll_It/Assets/Scripts/Project/Game/Common/View/GameView.cs
@@ -6,23 +6,13 @@ using UnityEngine.UI;
 using UniRx;
 using UDL.Core;
 using UDL.Core.UI;
-using Project.Screen.Result.View;
-using Project.Game.Model;
-using UniRx;
 
 namespace Project.Game.View
 {
     public class GameView : AbstractView
     {
-        [SerializeField] MeshRenderer showObject;
         [SerializeField] Camera myMainCamera;
         [SerializeField] Camera myModelCamera;
-        [SerializeField] GameObject mapParent;
-        [SerializeField] GameObject objectReviewParent;
-
-        
-        public Subject<Unit> OnFinishMap = new Subject<Unit>();
-
         
     }
 }
---
+++ Roll_It/Assets/Scripts/Project/Game/CylinderObject/View/CylinderView.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UniRx;
+using UDL.Core;
+
+public class CylinderView : AbstractView
+{
+    [SerializeField] private GameObject CylinderGo;
+    [SerializeField] private GameObject LineGo;
+
+    [SerializeField] private Transform StarPosition;
+    [SerializeField] private Transform EndPosition;
+
+    private bool isOpen = false;
+
+    private void Start() {
+        this.isOpen = false;
+
+        // Open();
+        StartCoroutine(OpenAndClose());
+    }
+
+    public void Open()
+    {
+        this.isOpen = true;
+
+        this.CylinderGo.transform.DOLocalMoveZ(5,2f);
+        this.LineGo.transform.DOScaleZ(5,2f);
+        
+    }
+
+    public void Close()
+    {
+        this.isOpen = false;
+
+        this.CylinderGo.transform.DOLocalMoveZ(0.5f,2f);
+        this.LineGo.transform.DOScaleZ(1,2f);
+    }
+
+    IEnumerator OpenAndClose()
+    {
+        Open();
+
+        yield return new WaitForSeconds(6);
+
+        Close();
+    }
+}
