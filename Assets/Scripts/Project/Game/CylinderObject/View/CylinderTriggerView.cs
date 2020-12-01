6076bc7dacf4a690f6eb5fad66f4c00080c3058e
vfa-DungLN <vfa.dungnl@gmail.com>
2020-12-01 13:37:43 +0700

    #10543 - when Object below other Object still roll in until collider with that Object after roll out old position 	     - remove class dont use

---
+++ Assets/Scripts/Project/Game/CylinderObject/View/CylinderTriggerView.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CylinderTriggerView : MonoBehaviour
+{
+    [SerializeField] CylinderView cylinder;
+    private void OnTriggerEnter(Collider other)
+    {
+        this.cylinder.OnCylinderTriggerEnter(other);
+    }
+}
